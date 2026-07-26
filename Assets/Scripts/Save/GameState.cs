3b38f34dff25d4d51ce6e61771c98b1081867ff4
Stiven-Gjekaj <stivenagostingjekaj@gmail.com>
2026-07-26 16:23:44 +0000

    v0.1.40: restored the Linq using needed by the session save format

--- Assets/Scripts/Save/GameState.cs
+++ Assets/Scripts/Save/GameState.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using MoonSharp.Interpreter;
 
 /// <summary>
